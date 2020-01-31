4236a2f214b64054b64cbd007df5fdba11344434
honghong <jjins23@gmail.com>
2020-01-31 19:42:30 +0900

    사업자 조회

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
+    path('business/', include('business.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
