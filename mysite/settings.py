1d083aa332dd80be6ab5d9b287436d77a989bda7
ksw <ksw1016v@gmail.com>
2020-01-31 20:39:28 +0900

    start^^

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'business.apps.BusinessConfig',
-    'polls.apps.PollsConfig',
+    'business',
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
